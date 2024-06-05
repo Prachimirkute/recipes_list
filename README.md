9468b3a881f7bac6ab6b4eaa85aa8c6dfb9afd3d
Prachimirkute <125886153+Prachimirkute@users.noreply.github.com>
2024-06-05 21:40:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+## Installation
+1. Clone the repository
+1. Run `npm install`
+1. Run `npm run dev` to start the application
+
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/)
---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="px-3 py-2 rounded border-2 text-white border-purple-600 bg-purple-500 hover:bg-purple-600 transition-colors"
+  >
+    <slot>YouTube</slot>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  href: String
+})
+</script>
